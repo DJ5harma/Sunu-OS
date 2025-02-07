6ecf8f22d6db438a69848507ab12e93b0e9bf862
DJ5harma <sunubhargav2004@gmail.com>
2025-02-07 05:51:51 +0530

    added multiple windows functionality with dragging, resizing and focusing

--- src/WindowManager/WindowManager.tsx
+++ src/WindowManager/WindowManager.tsx
@@ -1,47 +1,81 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 import Window from "./components/Window";
 
 const context = createContext<{
-	entities: { [key: number]: ReactNode };
-	addEntity: (_component: ReactNode) => void;
+	entities: {
+		[key: number]: {
+			minimized: boolean;
+			component: ReactNode;
+			z_index: number;
+		};
+	};
+	addEntity: (component: ReactNode) => void;
+	destroyEntity: (key: number) => void;
+	pushEntityForward: (key: number) => void;
 }>({
 	entities: {},
 	addEntity: (_component: ReactNode) => {},
+	destroyEntity: (_key: number) => {},
+	pushEntityForward: (_key: number) => {},
 });
+export const useWindowManager = () => useContext(context);
 
 export default function WindowManager() {
-	const [window_counter, setWindowCounter] = useState(0);
+	const [window_counter, set_window_counter] = useState(0);
 
-	const [entities, setEntities] = useState<{ [key: number]: ReactNode }>({});
+	const [entities, setEntities] = useState<{
+		[key: number]: {
+			minimized: boolean;
+			component: ReactNode;
+			z_index: number;
+		};
+	}>({});
 
 	function addEntity(component: ReactNode) {
-		setWindowCounter(window_counter + 1);
-		entities[window_counter] = component;
-		setEntities({ ...entities });
-
-		console.log(entities);
+		setEntities((prevEntities) => ({
+			...prevEntities,
+			[window_counter]: {
+				component,
+				minimized: false,
+				z_index: window_counter,
+			},
+		}));
+		set_window_counter((prev) => prev + 1);
 	}
 
 	function destroyEntity(key: number) {
-		delete entities[window_counter];
-		setEntities({ ...entities });
+		setEntities((prevEntities) => {
+			const newEntities = { ...prevEntities };
+			delete newEntities[key]; // a fresh object is created
+			return newEntities;
+		});
 	}
 
-	function renderEntities() {}
+	function pushEntityForward(key: number) {
+		setEntities((prevEntities) => {
+			prevEntities[key].z_index = window_counter;
+			return { ...prevEntities };
+		});
+
+		set_window_counter((prev) => prev + 1);
+		console.log(entities);
+	}
 
 	return (
-		<div>
-			<button onClick={() => addEntity(<div>Hello {window_counter}</div>)}>
-				Click
-			</button>
+		<context.Provider
+			value={{ entities, addEntity, destroyEntity, pushEntityForward }}
+		>
+			<div>
+				<button onClick={() => addEntity(<div>Hello {window_counter}</div>)}>
+					Click
+				</button>
 
-			{Object.values(entities).map((component, key) => {
-				return (
-					<Window key={key}>
-						<div>{component}</div>;
+				{Object.entries(entities).map(([key, value]) => (
+					<Window myKey={Number(key)} z_index={value.z_index}>
+						{value.component}
 					</Window>
-				);
-			})}
-		</div>
+				))}
+			</div>
+		</context.Provider>
 	);
 }
--- src/WindowManager/components/Window.tsx
+++ src/WindowManager/components/Window.tsx
@@ -1,19 +1,72 @@
 import { ReactNode } from "react";
+import { FiMaximize2 } from "react-icons/fi";
+import { RxCross2 } from "react-icons/rx";
+import { VscChromeMinimize } from "react-icons/vsc";
+import { Rnd } from "react-rnd";
+import { useWindowManager } from "../WindowManager";
 
-export default function Window({ children }: { children: ReactNode }) {
+export default function Window({
+	children,
+	myKey,
+	z_index,
+}: {
+	children: ReactNode;
+	myKey: number;
+	z_index: number;
+}) {
+	const { destroyEntity, pushEntityForward } = useWindowManager();
+
 	return (
-		<div
-			style={{
+		<Rnd
+			default={{
+				x: window.innerWidth / 4,
+				y: window.innerHeight / 4,
 				width: window.innerWidth / 2,
 				height: window.innerHeight / 2,
-				position: "absolute",
-				top: window.innerHeight / 4,
-				left: window.innerWidth / 4,
-				//
-				border: "solid red",
 			}}
+			dragHandleClassName="drag-handler"
+			style={{ border: "red solid", zIndex: z_index }}
+			onMouseDown={() => pushEntityForward(myKey)}
 		>
+			<nav
+				style={{
+					background: "#333",
+					color: "#fff",
+					display: "flex",
+					justifyContent: "space-between",
+					alignItems: "center",
+					userSelect: "none",
+					height: 50,
+					padding: "0 15px 0 0",
+				}}
+			>
+				<div
+					className="drag-handler"
+					style={{
+						width: "100%",
+						height: "100%",
+						display: "flex",
+						alignItems: "center",
+						paddingLeft: 15,
+					}}
+				>
+					<p>Component Name</p>
+				</div>
+				<div style={{ display: "flex", gap: 15, alignItems: "center" }}>
+					<VscChromeMinimize
+						size={20}
+						style={{ cursor: "pointer" }}
+						onClick={() => alert("Minimize clicked!")}
+					/>
+					<FiMaximize2 size={20} style={{ cursor: "pointer" }} />
+					<RxCross2
+						size={20}
+						style={{ cursor: "pointer" }}
+						onClick={() => destroyEntity(myKey)}
+					/>
+				</div>
+			</nav>
 			{children}
-		</div>
+		</Rnd>
 	);
 }
